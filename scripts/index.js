42aeaf3c509f01049df5a1994045d0ead1da3de6
Rita Lapina <margarit.lapina@yandex.ru>
2024-03-08 17:03:11 +0400

    first review

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const mainContent= document.querySelector('.content');
+const cardContainer = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+//  Функция создания карточки
+function createCard(dataCard, cardLink) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  cardElement.querySelector('.card__title').textContent = dataCard;
+  cardElement.querySelector('.card__image').src = cardLink;
+  
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {
+    deleteCard(cardElement);
+  });
+  
+  return cardElement; 
+}
 
-// @todo: Функция удаления карточки
+//  Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
-// @todo: Вывести карточки на страницу
+initialCards.forEach(function(card) {
+  cardContainer.append(createCard(card.data, card.link));
+});
