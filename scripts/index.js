07ecee044ff73d22480d48d7bf4aca0e300b12fe
Rita Lapina <margarit.lapina@yandex.ru>
2024-03-11 13:29:27 +0400

    второе ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,28 @@
 const mainContent= document.querySelector('.content');
 const cardContainer = document.querySelector('.places__list');
 
-//  Функция создания карточки
-function createCard(dataCard, cardLink) {
+
+function createCard(listItem, deleteHandler) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = listItem.name;
+  cardElement.querySelector('.card__image').src = listItem.link;
+  cardElement.querySelector('.card__image').alt = listItem.name;
   
-  cardElement.querySelector('.card__title').textContent = dataCard;
-  cardElement.querySelector('.card__image').src = cardLink;
   
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function() {
-    deleteCard(cardElement);
+ 
+  deleteButton.addEventListener('click', function () {
+    deleteHandler(cardElement);
   });
   
   return cardElement; 
 }
 
-//  Функция удаления карточки
 function deleteCard(cardElement) {
     cardElement.remove();
 }
 
 initialCards.forEach(function(card) {
-  cardContainer.append(createCard(card.data, card.link));
+  cardContainer.append(createCard(card, deleteCard));
 });
